89f490534afb59a912ea0ae8762e0e28072d06fa
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2025-02-13 23:24:39 +0100

    blog week 1 bij gevuld

--- portfolio/src/views/blogs.vue
+++ portfolio/src/views/blogs.vue
@@ -14,30 +14,31 @@ const selectedWeek = week.data.filter(item => {
 </script>
 
 <template>
-    <div class="flex flex-col items-center">
-      <div v-for="week in selectedWeek" class="flex flex-col w-full bg-white p-8 m-4 shadow-lg rounded-2xl">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <div class="p-4 bg-gray-100 rounded-lg">
-            <h3 class="text-lg font-bold mb-2">Monday</h3>
-            <div v-html="week.Monday"></div>
-          </div>
-          <div class="p-4 bg-gray-200 rounded-lg">
-            <h3 class="text-lg font-bold mb-2">Tuesday</h3>
-            <div v-html="week.Tuesday"></div>
-          </div>
-          <div class="p-4 bg-gray-100 rounded-lg">
-            <h3 class="text-lg font-bold mb-2">Wednesday</h3>
-            <div v-html="week.Wednesday"></div>
-          </div>
-          <div class="p-4 bg-gray-200 rounded-lg">
-            <h3 class="text-lg font-bold mb-2">Thursday</h3>
-            <div v-html="week.Thursday"></div>
-          </div>
-          <div class="p-4 bg-gray-100 rounded-lg">
-            <h3 class="text-lg font-bold mb-2">Friday</h3>
-            <div v-html="week.Friday"></div>
-          </div>
+  <div class="flex flex-col items-center">
+    <div v-for="week in selectedWeek" class="flex flex-col w-full bg-white p-8 m-4 shadow-lg rounded-2xl">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="p-4 bg-gray-100 rounded-lg h-90 overflow-y-auto">
+          <h3 class="text-lg font-bold mb-2">Monday</h3>
+          <div v-html="week.Monday"></div>
+        </div>
+        <div class="p-4 bg-gray-200 rounded-lg h-90 overflow-y-auto">
+          <h3 class="text-lg font-bold mb-2">Tuesday</h3>
+          <div v-html="week.Tuesday"></div>
+        </div>
+        <div class="p-4 bg-gray-100 rounded-lg h-90 overflow-y-auto">
+          <h3 class="text-lg font-bold mb-2">Wednesday</h3>
+          <div v-html="week.Wednesday"></div>
+        </div>
+        <div class="p-4 bg-gray-200 rounded-lg h-90 overflow-y-auto">
+          <h3 class="text-lg font-bold mb-2">Thursday</h3>
+          <div v-html="week.Thursday"></div>
+        </div>
+        <div class="p-4 bg-gray-100 rounded-lg h-90 overflow-y-auto">
+          <h3 class="text-lg font-bold mb-2">Friday</h3>
+          <div v-html="week.Friday"></div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+  
